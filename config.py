dd44f6791322e11bc4de24c2db967fbdd2e3c0ba
rhum1s <romain.souweine@hotmail.fr>
2015-04-11 01:17:17 +0200

    Config class working

--- config.py
+++ config.py
@@ -2,34 +2,31 @@
 # config.py
 # R. Souweine, 2015
 
+import sys
 import ConfigParser
 
-def read_config(config_file):
-    the_config = ConfigParser.ConfigParser()
-    the_config.read(config_file)
-    return the_config
 
-def map_config_section(config_object, section):
-    """
-    Reads a defined section of a config object.
-    NOTE: To read a boolean: Config.getboolean(section, option)
-    """
-    config_dict = {}
-    options = config_object.options(section)
-    for option in options:
-        try:
-            config_dict[option] = config_object.get(section, option)
-            if config_dict[option] == -1:
-                print("DEBUG: Skip: %s" % option)
-        except:
-            print("ERROR: Exception on %s!" % option)
-            config_dict[option] = None
-    return config_dict
+class Cfg():
+    def __init__(self, config_file):
+        """
+        Reads the config file and extracts params.
+        :param config_file: Text file in .ini format.
+        :return: An object containing all params.
+        """
+        self.config_file = config_file
+        self.config_parser = ConfigParser.ConfigParser()
+        self.config_parser.read(self.config_file)
+        self.sections = self.config_parser.sections()
+
+        # Retrieve PostgreSQL params
+        self.pg_host = self.config_parser.get('PostgreSQL', 'host')
+        self.pg_port = self.config_parser.get('PostgreSQL', 'port')
+        self.pg_lgn = self.config_parser.get('PostgreSQL', 'login')
+        self.pg_pwd = self.config_parser.get('PostgreSQL', 'password')
 
 if __name__ == "__main__":
-    
-    # Read config file
-    config = read_config("config.cfg")
-    cfg_pg = map_config_section(config, "PostgreSQL")
 
-    print cfg_pg
+    print "config.py - Running tests"
+    cfg = Cfg("config.cfg")
+    cfg.show()
+
