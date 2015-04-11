0eb3e101d894eaa4e4a94bc24d6286f30616c5fb
rhum1s <romain.souweine@hotmail.fr>
2015-04-11 03:34:55 +0200

    Add geo_select function and test for multiple connections avoidance

--- pg.py
+++ pg.py
@@ -1,16 +1,18 @@
 # config.py
 # R. Souweine, 2015
 
+# TODO: Use unittest (https://github.com/geopandas/geopandas/commit/5bae14a9f4aa56978e06485f6b19399a9b335686)
+
 import psycopg2 as pg
 import pandas.io.sql as psql
+import geopandas as gpd
 from config import Cfg
 
 
 class Pg():
     def __init__(self, config_file):
         """
-        :param config_file:
-        :return:
+        :param config_file: The .ini type configuration text file.
         """
         cfg = Cfg(config_file)  # Reads config file
 
@@ -25,16 +27,29 @@ class Pg():
         :param sql: The query
         :return: Pandas Dataframe
         """
-        # TODO: Test if creates multiple connections
         con = pg.connect(self.conn_string)
-        dataframe = psql.frame_query(sql, con)
+        dataframe = psql.read_sql(sql, con)
         con.close()
         return dataframe
+
+    def geo_select(self, sql, geom_col='geom'):
+        """
+        Like select function put also extract geometry in WKT thanks to GeoPandas.
+        """
+        # TODO: Extract XY?
+        con = pg.connect(self.conn_string)
+        geo_dataframe = gpd.read_postgis(sql, con, geom_col=geom_col)
+        con.close()
+        return geo_dataframe
 
 if __name__ == "__main__":
     print "pg.py - Running tests ..."
     db = Pg("config.cfg")
-    df = db.select("select gid, candidat, toponyme from bdcarthage.cours_d_eau limit 0;")
-    print df.to_string()
 
-
+    # Avoid multiple connections test
+    nb_conn1 = db.select("select sum(numbackends) as nb from pg_stat_database;").nb.values[0]
+    df = db.select("select gid, candidat, toponyme from bdcarthage.cours_d_eau limit 3;")
+    gdf = db.geo_select("select * from bdcarthage.cours_d_eau limit 3;")
+    nb_conn2 = db.select("select sum(numbackends) as nb from pg_stat_database;").nb.values[0]
+    if nb_conn2 <= nb_conn1:
+        print "- Avoid multiple connections: Success (%s->%s)." % (nb_conn1, nb_conn2)
