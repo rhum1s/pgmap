7e2f074acc8b2961daa49781bdec0d2599c00bba
rhum1s <romain.souweine@hotmail.fr>
2015-04-11 15:00:14 +0200

    Improve execute function with commits and notices

--- pg.py
+++ pg.py
@@ -1,7 +1,7 @@
 # config.py
 # R. Souweine, 2015
 
-# TODO: Use unittest (https://github.com/geopandas/geopandas/commit/5bae14a9f4aa56978e06485f6b19399a9b335686)
+# TODO: Use unittest (http://sametmax.com/un-gros-guide-bien-gras-sur-les-tests-unitaires-en-python-partie-2/)
 
 import psycopg2 as pg
 import pandas.io.sql as psql
@@ -15,19 +15,30 @@ class Pg():
         :param config_file: The .ini type configuration text file.
         """
         cfg = Cfg(config_file)  # Reads config file
-
         self.conn_string = "host=%s port=%s dbname=%s user=%s password=%s" % \
                            (cfg.pg_host, cfg.pg_port, cfg.pg_db, cfg.pg_lgn, cfg.pg_pwd)
 
-    def execute(self, sql):
+    def execute(self, sql, notices=False):
         """
         Executes query in PostgreSQL.
+        Can run multiple queries at a time.
+        Transactions work as everything will be rolled back if error.
+        :param sql: The query
+        :param notices: Print PostgreSQL notices if there are.
         """
-        # TODO: Seems to be able to exec multiple queries on one time.
-        # TODO: Be able to display outputs of execute queries.
         con = pg.connect(self.conn_string)
-        psql.execute(sql, con)
+        try:
+            psql.execute(sql, con)
+            con.commit()
+            con.close()
+        except psql.DatabaseError, e:
+            print "ERROR", e
         con.close()
+
+        # If notices print it
+        if len(con.notices) > 0 and notices is True:
+            for notice in con.notices:
+                print notice.replace('\n', '')
 
     def select(self, sql):
         """
@@ -46,7 +57,6 @@ class Pg():
         """
         Like select function put also extract geometry in WKT thanks to GeoPandas.
         """
-        # TODO: Extract XY?
         con = pg.connect(self.conn_string)
         geo_dataframe = gpd.read_postgis(sql, con, geom_col=geom_col)
         con.close()
@@ -56,11 +66,17 @@ if __name__ == "__main__":
     print "pg.py - Running tests ..."
     db = Pg("config.cfg")
 
-    # Avoid multiple connections test
+    # Number of connections at start
     nb_conn1 = db.select("select sum(numbackends) as nb from pg_stat_database;").nb.values[0]
-    db.execute("drop table if exists toto; create temporary table toto (gid integer);")
+
+    # NOTE: The next test will create table, not that good in another person database.
+    db.execute("drop table if exists toto; drop table if exists toto; create table public.toto(gid integer);", True)
     df = db.select("select gid, candidat, toponyme from bdcarthage.cours_d_eau limit 3;")
     gdf = db.geo_select("select * from bdcarthage.cours_d_eau limit 3;")
+
+    # Verify if each connections where closed
     nb_conn2 = db.select("select sum(numbackends) as nb from pg_stat_database;").nb.values[0]
     if nb_conn2 <= nb_conn1:
         print "- Avoid multiple connections: Success (%s->%s)." % (nb_conn1, nb_conn2)
+    else:
+        print "ERROR: During test, not all connections where closed (%s->%s)." % (nb_conn1, nb_conn2)
